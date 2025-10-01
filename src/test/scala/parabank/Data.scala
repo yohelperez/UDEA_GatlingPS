0463f60748836993c3d5e9906a62e0c239fe58bc
chrisystems <51771601+chrisystems@users.noreply.github.com>
2025-09-30 19:16:51 -0500

    Linea Base

---
+++ src/test/scala/parabank/Data.scala
@@ -0,0 +1,7 @@
+package parabank
+
+object Data{
+    val url= "https://parabank.parasoft.com/parabank/services/bank"
+    val username = "john"
+    val password = "demo"
+}
---
+++ src/test/scala/parabank/LoginTest.scala
@@ -0,0 +1,27 @@
+package parabank
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import parabank.Data._
+
+class LoginTest extends Simulation{
+
+  // 1 Http Conf
+  val httpConf = http.baseUrl(url)
+    .acceptHeader("application/json")
+    //Verificar de forma general para todas las solicitudes
+    .check(status.is(200))
+
+  // 2 Scenario Definition
+  val scn = scenario("Login").
+    exec(http("login")
+      .get(s"/login/$username/$password")
+       //Recibir información de la cuenta
+      .check(status.is(200))
+    )
+
+  // 3 Load Scenario
+  setUp(
+    scn.inject(rampUsersPerSec(5).to(15).during(30))
+  ).protocols(httpConf);
+}
