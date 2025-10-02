cd6467ee179d50355b15c884d9d97073e8b8db61
Duvan Ruiz <82849982+DuvanR0598@users.noreply.github.com>
2025-10-01 22:16:09 -0500

    Update LoginTest.scala

--- src/test/scala/parabank/LoginTest.scala
+++ src/test/scala/parabank/LoginTest.scala
@@ -23,28 +23,10 @@ class LoginTest extends Simulation{
 
   // 3 Load Scenario
   setUp(
-    // Escenario 1: Carga normal (100 usuarios concurrentes)
-    scn.inject(
-      nothingFor(5.seconds), // Tiempo de espera inicial
-      rampUsersPerSec(1).to(100).during(2.minutes), // Rampa hasta 100 usuarios
-      constantUsersPerSec(100).during(5.minutes) // Mantener 100 usuarios por 5 minutos
-    ).protocols(httpConf),
-    
-    // Escenario 2: Carga pico (200 usuarios concurrentes)
-    scn.inject(
-      nothingFor(10.seconds),
-      rampUsersPerSec(1).to(200).during(1.minute),
-      constantUsersPerSec(200).during(3.minutes)
-    ).protocols(httpConf)
-  )
-    .assertions(
-      // Aserciones basadas en los criterios de aceptación
-      global.responseTime.percentile(95).lt(2000), // 95% de requests < 2s
-      global.responseTime.percentile(99).lt(5000), // 99% de requests < 5s
-      global.failedRequests.percent.lt(1.0) // Menos del 1% de errores
-    )
-    .maxDuration(10.minutes)
+    scn.inject(rampUsersPerSec(5).to(15).during(30))
+  ).protocols(httpConf);
 }
+
 
 
 
