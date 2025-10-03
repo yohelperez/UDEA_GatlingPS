eb3503233046583acd34398e76f6c526cd920f29
Duvan Ruiz <82849982+DuvanR0598@users.noreply.github.com>
2025-10-02 22:47:26 -0500

    Update Data.scala

--- src/test/scala/parabank/Data.scala
+++ src/test/scala/parabank/Data.scala
@@ -4,11 +4,9 @@ object Data{
     val url= "https://parabank.parasoft.com/parabank/services/bank"
     val username = "john"
     val password = "demo"
-    val accounts = Array(
-    "12345","12456","12567","12678","12789","12900","13011",
-    "13122","13233","13344","54321")
     
 }
+
 
 
 
